e4f6a953369814372192f01e832e9041359b723a
Abhi123mondal <89828330+Abhi123mondal@users.noreply.github.com>
2021-11-25 18:20:55 +0530

    Add files via upload

---
+++ thh.js
@@ -0,0 +1,13 @@
+var x=5;
+var y=3;
+function all(){
+    add=x+y;
+    div=x/y;
+    sub=x-y;
+    mul=x*y;
+    document.write("add is the result:"+ add+"<br>");
+    document.write("division is the result:"+ div+"<br>");
+    document.write("substracion is the result:"+ sub+"<br>");
+    document.write("multiplication is the result:"+ mul+"<br>");
+}
+all()
